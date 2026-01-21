4346394e7dd412b96296d225f04d20641821d0f9
Zaheem <zaheem@segfault.net>
2026-01-21 15:38:15 +0530

    initial commit

---
+++ print.java
@@ -0,0 +1,7 @@
+public class printfuntion{
+    public static void main(String[] args){
+        system.out.println("github");
+        system.out.println("git track");
+        system.out.println("2nd commit");
+    }
+}
